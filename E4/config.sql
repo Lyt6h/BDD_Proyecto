501accd0eaf60fa0a318f28b1e2ec3ae394c6331
MatiasV15 <matiaspalomerac@gmail.com>
2025-11-19 21:51:34 -0300

    0.1.1

--- E4/config.sql
+++ E4/config.sql
@@ -1,6 +1,6 @@
 -- 1.a) Indices secundarios
-CREATE INDEX RUN ON Persona(RUN);
-CREATE INDEX RUT ON InstituciondeSalud(RUT);        -- Verificar si esto es correcto
+CREATE INDEX Persona_RUN ON Persona(RUN);
+CREATE INDEX InstituciondeSalud_RUT ON InstituciondeSalud(RUT);        -- Verificar si esto es correcto
 
 -- 1.b) INdice primario
 ALTER TABLE "Agenda" ADD CONSTRAINT PK_agenda PRYMARY KEY ("ID", "Fecha", "Hora");
@@ -8,7 +8,7 @@ ALTER TABLE "Agenda" ADD CONSTRAINT PK_agenda PRYMARY KEY ("ID", "Fecha", "Hora"
 -- 1.c) Transacciones
 BEGIN;
 
-
+INSERT INTO ""
 
 COMMIT;
 
--- E4/index.php
+++ E4/index.php
@@ -1,5 +1,10 @@
 <?php
-
+function conectarBD() {
+    $host = 'localhost';
+    $dbname = 'bdd';  // nombre de usuario
+    $usuario = 'username'; // nombre de usuario
+    $clave = 'contraseña'; //numero de alumno
+}
 
 
 
